e845d7b3aafdcd3f5c17a8c768028b079ef3c463
Daniel Koch <k@kdb.gmbh>
2021-03-09 07:21:18 +0100

    Initial commit

---
+++ src/main/java/gmbh/kdb/svcdemo/SvcdemoApplication.java
@@ -0,0 +1,13 @@
+package gmbh.kdb.svcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcdemoApplication.class, args);
+	}
+
+}
